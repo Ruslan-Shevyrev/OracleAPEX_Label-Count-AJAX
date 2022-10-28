e42aff3a87bebad20250171559902198b769d72d
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2022-10-28 16:58:33 +0300

    first commit

---
+++ AJAX_COUNT_LABEL_APEX.js
@@ -0,0 +1,26 @@
+function addNavigationBadgeLoader() {
+
+
+	setTimeout(function () {
+
+		var navTabs = document.querySelector('.apex-rds-container');
+
+		if (navTabs) {
+
+			var navTabsList = navTabs.querySelectorAll('ul > li .js-three-quarters-loader');
+
+			for (var i = 0; i < navTabsList.length; i++) {
+				navTabsList[i].insertAdjacentHTML('beforeend', '<span class="three-quarters-loader"></span>');
+			}
+
+		}
+
+		var navList = document.querySelectorAll('.js-three-quarters-loader + a');
+
+		for (var i = 0; i < navList.length; i++) {
+			navList[i].insertAdjacentHTML('beforeend', '<span class="three-quarters-loader"></span>');
+		}
+
+	}, 100);
+
+}
