d21921678e7847658de3b38bbe2559b60e8bb61c
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2022-10-28 17:24:15 +0300

    add AJAX_COUNT_LABEL_APEX

--- AJAX_COUNT_LABEL_APEX.js
+++ AJAX_COUNT_LABEL_APEX.js
@@ -21,3 +21,84 @@ function addNavigationBadgeLoader(){
 
 	}, 100);
 };
+
+function removeBadgeLoader(itemClass){
+
+	setTimeout(function(){
+
+		var loaderElement = document.querySelector('.' + itemClass + ' + a .three-quarters-loader');
+
+		if (loaderElement){
+
+			if (typeof loaderElement.remove == 'function'){
+				loaderElement.remove();
+			} 
+			else{
+				loaderElement.outerHTML = '';
+			}
+		}
+	}, 100);
+};
+
+function removeBadgeTabsLoader(itemClass) {
+
+	setTimeout(function(){
+		var loaderElement = document.querySelector('.' + itemClass + ' > .three-quarters-loader');
+
+		if (loaderElement){
+
+			if (typeof loaderElement.remove == 'function'){
+				loaderElement.remove();
+			}
+			else{
+				loaderElement.outerHTML = '';
+			}
+		}
+	}, 100);
+};
+
+
+function addNavigationBadge(serviceName) {
+
+	apex.server.process(
+		serviceName,
+		{
+			x01: 1
+		},
+		{
+			success: function (pData) {
+			
+				var json = JSON.parse(pData);
+
+				if (json.length && json.length == 0) {
+					return false;
+				}
+
+				for (var i = 0; i < json.length; i++) {
+
+					for (var itemClass in json[i]) {
+
+						if (json[i][itemClass] > 0) {
+
+							var htmlBadge = '<span class="a-TreeView-badge">' + json[i][itemClass] + '</span>';
+
+							if (document.querySelector('.' + itemClass + ' + a')) {
+								document.querySelector('.' + itemClass + ' + a').insertAdjacentHTML('beforeend', htmlBadge);
+							}
+
+							if (document.querySelector('.' + itemClass + ' > .three-quarters-loader')) {
+								document.querySelector('.' + itemClass).insertAdjacentHTML('beforeend', htmlBadge);
+							}
+						}
+
+						removeBadgeLoader(itemClass);
+
+						removeBadgeTabsLoader(itemClass);
+						
+					}
+				}
+			},
+			dataType: "text"
+		}
+	);
+};
