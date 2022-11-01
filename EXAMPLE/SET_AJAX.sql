9fc8968a91a12d6711459e5588ec0c426c8f1079
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2022-11-01 14:17:56 +0300

    add SET_AJAX

--- EXAMPLE/SET_AJAX.sql
+++ EXAMPLE/SET_AJAX.sql
@@ -1,6 +1,7 @@
 BEGIN
     HTP.p ('<script type="text/javascript">');
     HTP.p (   'addNavigationBadgeLoader();');
+    HTP.p (   'addNavigationBadge(''COUNT_TEST'');');
     --register your AJAX Process here
     HTP.p ('</script>');
 END;
